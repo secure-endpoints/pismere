06d9d1547a0b98e2500c68dd3a0c1b0c0941987f
Jeffrey Altman <jaltman@secure-endpoints.com>
2011-04-28 23:02:38 -0400

    add some convenience batch files
    Change-Id: Ibf426df93f4e8a4448db764fe2ee2160869464ed

---
+++ src/build_official.bat
@@ -0,0 +1,10 @@
+@echo off
+set AWK=gawk
+set APPVER=5.0
+set TARGETOS=WINNT
+SET _WIN32_IE=0x0500
+SET _WIN32_WINNT=0x0500
+unset KH_AFSPATH KH_KFWPATH KFWSDKDIR
+REM OFFICIAL or PRERELEASE or PRIVATE
+set KH_RELEASE=OFFICIAL
+cmd.exe /c c:\perl64\bin\perl.exe ..\scripts\build.pl --tools /nologo BUILD_KFW=1 BUILD_OFFICIAL=1 DEBUG_SYMBOL=1 %1 %2 %3 %4 %5 %6 %7 %8 %9
