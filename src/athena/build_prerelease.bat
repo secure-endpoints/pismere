ac4738aa395ae57d086fc447e13d81a13d637521
Jeffrey Altman <jaltman@secure-endpoints.com>
2011-05-03 17:53:19 -0400

    SE: update convenience batch files
    make batch files compatible with cmd.exe
    
    Change-Id: I1ecb7492762a7115de9ab574e71392a83e18920e

--- src/athena/build_prerelease.bat
+++ src/athena/build_prerelease.bat
@@ -4,7 +4,9 @@ set APPVER=5.0
 set TARGETOS=WINNT
 SET _WIN32_IE=0x0500
 SET _WIN32_WINNT=0x0500
-unset KH_AFSPATH KH_KFWPATH KFWSDKDIR
+set KH_AFSPATH=
+set KH_KFWPATH=
+set KFWSDKDIR=
 REM OFFICIAL or PRERELEASE or PRIVATE
 set KH_RELEASE=PRERELEASE
-cmd.exe /c c:\perl64\bin\perl.exe ..\scripts\build.pl --tools /nologo BUILD_KFW=1 DEBUG_SYMBOL=1 %1 %2 %3 %4 %5 %6 %7 %8 %9
+cmd.exe /c perl.exe ..\scripts\build.pl --tools /nologo BUILD_KFW=1 DEBUG_SYMBOL=1 %1 %2 %3 %4 %5 %6 %7 %8 %9
